b2615849ea73a382bb65f9b3fa351977f86a4ed5
Q M <qdevelop@hotmail.com>
2021-09-27 21:47:33 +0900

    add 境界条件のファイル

--- 2d_poisson_dirichlet/mesher.jl
+++ 2d_poisson_dirichlet/mesher.jl
@@ -4,20 +4,29 @@ N_elements = 5 # 1辺あたりの同じ形状の要素数
 N_nodes = N_elements + 1 # 1辺あたりの節点数
 nodes = DataFrame(x=Float64[], y=Float64[])
 
+boundary = DataFrame(node=Int64[], val=Float64[])
+
 for iy = 1:N_nodes
     for ix = 1:N_nodes
-        push!(nodes, ((ix-1)/N_elements, (iy-1)/N_elements))
+        x = (ix - 1) / N_elements
+        y = (iy - 1) / N_elements
+        push!(nodes, (x, y))
+
+        if x == 1 || x == 0 || y == 0 || y == 1
+            push!(boundary, (size(nodes,1), 0.0))
+        end
     end
 end
 
 connectivity = DataFrame(node1=Int64[], node2=Int64[], node3=Int64[])
 for iy = 1:N_elements
     for ix = 1:N_elements
-        i = (iy-1)*N_nodes+ix
-        push!(connectivity,(i,i+1,i+N_nodes))
-        push!(connectivity,(i+1,i+N_nodes+1,i+N_nodes))
+        i = (iy - 1) * N_nodes + ix
+        push!(connectivity, (i, i + 1, i + N_nodes))
+        push!(connectivity, (i + 1, i + N_nodes + 1, i + N_nodes))
     end
 end
 
 CSV.write("2d_poisson_dirichlet/connectivity.csv", connectivity)
 CSV.write("2d_poisson_dirichlet/nodes.csv", nodes)
+CSV.write("2d_poisson_dirichlet/bc.csv", boundary)
